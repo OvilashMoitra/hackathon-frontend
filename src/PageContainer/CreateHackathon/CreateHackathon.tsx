06d3a274a74a088237ca54949c80a03fc60742d4
Ovilash Moitra <ovilashmoitraofficial@gmai.com>
2022-10-07 23:34:25 +0600

    environment setup

---
+++ src/PageContainer/CreateHackathon/CreateHackathon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CreateHackathon.scss'
+const CreateHackathon = () => {
+    return (
+        <div>
+            <p>Create Hackathon</p>
+        </div>
+    );
+};
+
+export default CreateHackathon;
---
+++ src/PageContainer/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.scss'
+const Home = () => {
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
